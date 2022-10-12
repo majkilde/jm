c0938aa3a112ee6a3739267b93cc5928d39149af
Jakob Majkilde <jakob@majkilde.dk>
2022-10-12 19:50:10 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿
+namespace JM // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}
